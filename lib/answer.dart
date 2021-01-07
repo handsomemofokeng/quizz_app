3684b3929a16018db0923f07895dcb6637b44a99
Handsome Mofokeng <handsomem23@hotmail.com>
2021-01-07 23:43:59 +0200

    added answer.dart file

---
+++ lib/answer.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: RaisedButton(
+        child: Text("Option 1"),
+        onPressed: () {},
+      ),
+    );
+  }
+}
